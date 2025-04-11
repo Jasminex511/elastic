31d1ff1bd49a4beea79158238f9eb6123a599ced
Jasminex511 <jasmine1999511@gmail.com>
2025-04-11 16:43:20 -0400

    fix index errors.

--- index_data.py
+++ index_data.py
@@ -1,6 +1,5 @@
 from elasticsearch import Elasticsearch
 from typing import List
-from tqdm import tqdm
 
 from config import INDEX_NAME
 from db import collection
@@ -11,7 +10,7 @@ def index_data(documents: List[dict]):
     es = get_es_client(max_retries=5, sleep_time=5)
     _ = _create_index(es=es, index_name=INDEX_NAME)
     _ = _insert_documents(es=es, index_name=INDEX_NAME, documents=documents)
-    print(f'Indexed {len(documents)} documents into Elasticsearch index {INDEX_NAME}')
+    print('Indexing completed!')
 
 def _create_index(es: Elasticsearch, index_name: str) -> dict:
     es.indices.delete(index=index_name, ignore_unavailable=True)
@@ -19,9 +18,11 @@ def _create_index(es: Elasticsearch, index_name: str) -> dict:
 
 def _insert_documents(es: Elasticsearch, index_name: str, documents: List[dict]) -> dict:
     operations = []
-    for doc in tqdm(documents, total=len(documents), desc='Indexing documents'):
+    for doc in documents:
+        doc["_id"] = str(doc["_id"])
         operations.append({'index': {'_index': index_name}})
         operations.append(doc)
+    print(f'Indexing {len(operations)} documents into Elasticsearch index {INDEX_NAME}')
     return es.bulk(operations=operations)
 
 
